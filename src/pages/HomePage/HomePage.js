acfaef7792f0f8725bd2838eafa7e94e7b0c0115
leonardopottmayer <pottmayerleonardo@gmail.com>
2022-10-21 19:48:41 -0300

    Added pottmayerDevApi post to projectsAccess

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import styles from "./HomePage.module.css";
 
 import api from "../../services/api";
+import pottmayerDevApi from "../../services/pottmayerDevApi";
 
 import PhraseComp from "../../components/Phrase/PhraseComp";
 import Credits from "../../components/Credits/Credits";
@@ -12,6 +13,10 @@ const HomePage = () => {
 
   const fetchApi = async () => {
     const response = await api.get("phrase/randomPhrase");
+
+    const projectAccessPostResponse = await pottmayerDevApi.post("/projectsAccess", {
+      projectName: "MTV API"
+    });
 
     if (response.data.result) {
       setPhrase(response.data.result);
