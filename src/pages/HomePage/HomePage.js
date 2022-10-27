b05d7a5d6fbf4653308c48288be0fd661e68b8c0
leonardopottmayer <pottmayerleonardo@gmail.com>
2022-10-27 20:56:59 -0300

    Another try

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,27 +9,26 @@ import PhraseComp from "../../components/Phrase/PhraseComp";
 import Credits from "../../components/Credits/Credits";
 
 const HomePage = () => {
-  const neverChanges = 1;
   const [phrase, setPhrase] = useState(null);
 
-  const fetchApi = async () => {
-    const response = await api.get("phrase/randomPhrase");
-
-    const projectAccessPostResponse = await pottmayerDevApi.post(
-      "/projectsAccess",
-      {
-        projectName: "MTV",
-      }
-    );
-
-    if (response.data.result) {
-      setPhrase(response.data.result);
-    }
-  };
-
   useEffect(() => {
+    const fetchApi = async () => {
+      const response = await api.get("phrase/randomPhrase");
+
+      const projectAccessPostResponse = await pottmayerDevApi.post(
+        "/projectsAccess",
+        {
+          projectName: "MTV",
+        }
+      );
+
+      if (response.data.result) {
+        setPhrase(response.data.result);
+      }
+    };
+
     fetchApi();
-  }, [neverChanges]);
+  }, []);
 
   return (
     <div className={styles.main}>
